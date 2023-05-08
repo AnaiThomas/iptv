daaf60685cace096ac2b611cc400444285329148
LuisCarrizo <lcarrizo2012@gmail.com>
2023-05-07 21:56:40 -0300

    first

---
+++ include/pageError.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<meta name="author" content="Luis Carrizo">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Error</title>
+
+
+<link href="/libs/bootstrap/v431/css/bootstrap.min.css" rel="stylesheet">
+<link href="/libs/font-awesome/v572/css/all.min.css" rel="stylesheet">
+<link href="/libs/noty/v3/noty.css" rel="stylesheet">
+<link href="/libs/noty/v3/themes/metroui.css" rel="stylesheet">
+
+<link href="/libs/wikired/master.css" rel="stylesheet">
+
+ 	<style type="text/css">
+		body,div,table,thead,tbody,tfoot,tr,th,td,p { 
+      font-family:"Calibri"; 
+      font-size:small 
+    }
+		a.comment-indicator:hover + comment { background:#ffd; position:absolute; display:block; border:1px solid black; padding:0.5em;  } 
+		a.comment-indicator { background:red; display:inline-block; border:1px solid black; width:0.5em; height:0.5em;  } 
+		comment { display:none;  } 
+
+    .wrCenter { 
+/*      left: 0;
+      right: 0;*/
+      /*top: 60px;*/
+      margin-left: auto;
+      margin-right: auto;
+      /*position: absolute;*/
+    }
+    .wrP0 {
+      padding: 0px !important;
+      line-height: 17px !important;
+    }
+    .wrP1 {
+      padding: 4px !important;
+    }
+    .wrP2 {
+      padding: 8px !important;
+    }
+    .wrP3 {
+      padding: 16px !important;
+    }
+    .wrP4 {
+      padding: 24px !important;
+    }
+    .wrP5 {
+      padding: 48px !important;
+    }
+	</style>
+
+</head>
+
+<body id="page-top">  
+
+	<!-- begin page header --> 
+	<header>
+		<!-- Navigation-->                                            <!-- justify-content-between -->
+		<nav class="navbar navbar-expand-lg navbar-darkw rLight wrBkg fixed-top "  id="mainNav">
+			<div class="navbar-brand">
+        <a  class="navbar-brand" href="/index.php">
+          <i class="fas fa-home"></i>
+        </a>
+        <span class="font-weight-bold font-italic " >Portal de Presupuesto Secretaría de Energia</span>
+      </div>	
+		</nav>
+	</header>
+
+	
+
+  <main role="main"  id="mainContainer"> <!-- class="mt-5 pt-5" --> 
+    <div class="container-fluid">
+      <div class="row pt-5">
+        <div class="col text-center"> 
+          <h1>La página que intentás abrir no está en el servidor</h1>
+        </div>            <!-- end col  -->
+      </div>            <!-- end row  -->
+      <div class="row pt-5">
+        <div class="col-2 offset-5"> 
+          <a class="btn btn-primary"  role="button" href="/index.php" target="_self">ir al inicio</a>
+        </div>            <!-- end col  -->
+      </div>            <!-- end row  -->
+    </div>            <!-- end container-fluid-->
+  </main> 
+
+  <footer>
+    
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- core js -->
+    <script src="/libs/jquery/jquery.min.js"></script>
+    <script src="/libs/bootstrap/v431/js/bootstrap.bundle.min.js"></script>
+    <script src="/libs/noty/v3/noty.min.js"></script>
+    <script src="/libs/wikired/master.js"></script>
+
+  </footer> 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+//phpinfo();
+
+require  $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+echo 'fin';
